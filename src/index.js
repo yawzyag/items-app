a781522196f9693a13e61c0e18b7b44339aad0fb
Yesid <yesid.yag@gmail.com>
2018-12-21 14:46:45 -0500

    database

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+// Database
+const db = require("./config/database");
+
+//Test db
+db.authenticate()
+  .then(() => console.log("database connected..."))
+  .catch(err => console.log(err));
+
+const app = express();
+
+// Index route
+app.get("/", (req, res) => res.send("hello"));
+
+const PORT = process.env.PORT || 3300;
+
+app.listen(PORT, console.log(`Server started on port: ${PORT}`));
