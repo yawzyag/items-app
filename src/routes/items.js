804346bf74061053bdd20b16772317ae909ceb62
Yesid <yesid.yag@gmail.com>
2018-12-21 16:20:26 -0500

    add item route

--- src/routes/items.js
+++ src/routes/items.js
@@ -1,8 +1,40 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../config/database");
+const Item = require("../models/Item");
 
+// get item list
 router.get("/", (req, res) => {
-  res.send("items page");
+  Item.findAll()
+    .then(items => {
+      console.log(items);
+      res.sendStatus(200);
+    })
+    .catch(err => console.log(err));
+});
+
+// Add item
+router.get("/add", (req, res) => {
+  const data = {
+    item_name: "maiz",
+    item_cost: "30000",
+    item_units: "10",
+    item_unit_price: "0"
+  };
+
+  let { item_name, item_cost, item_units, item_unit_price } = data;
+  item_unit_price = +item_cost / item_units;
+
+  //   console.log(item_unit_price);
+
+  Item.create({
+    item_name,
+    item_cost,
+    item_units,
+    item_unit_price
+  })
+    .then(item => res.redirect("/items"))
+    .catch(err => console.log(err));
 });
 
 module.exports = router;
